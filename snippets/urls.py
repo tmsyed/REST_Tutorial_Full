8ff0ccdde406784a5c3cb84a208ecf6bbfce1efc
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2020-06-14 15:47:33 -0400

    Tutorial 2 - Using basic Response and Request objects
    Configured API endpoints using api decorators from rest framework as well as using url format suffixes for various URL formats

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('', views.snippet_list),
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
