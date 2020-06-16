a854d79dcc7af2e741fc11e54e3cb054bb8451df
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2020-06-16 16:09:16 -0400

    Included User model into app
    Added functionality from the built in User model provided by Django.
    
    Configured all aspects (views, urls, migrations, and serializers) to include User models
    
    Also added a owner field to snippets to link to user who created the snippet.

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
 from rest_framework import generics
 
 
@@ -7,7 +8,20 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
